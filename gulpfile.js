5de668244cc9f388f7534825cc36ca7cdc44afbb
root <root@Kenny.localdomain>
2023-11-07 16:22:35 -0500

    integrating Flask

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 //Initialize the modules from the npm packages
-const { src, dest, watch, series } = require('gulp');
+const gulp= require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -7,29 +7,32 @@ const cssnano = require('cssnano');
 const babel = require('gulp-babel');
 const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
+const uglifycss = require('gulp-uglifycss');
 
 //Sass Task
-function scssTask() {
-    return src('app/scss/style.scss', { sourcemaps: true })
-    //sourcemaps for debugging and viewing code
-        .pipe(sass()) //running sass
-        .pipe(postcss([autoprefixer(), cssnano()])) 
-        //autoprefixer will add browser prefixes to support old browsers
-        //cssnano minfies css file
-        .pipe(dest('dist', { sourcemaps: '.' }));
-        //puts file into dist. '.' means same location as dist
-}
+gulp.task('scss', function(){
+  return gulp.src('./app/scss/style.scss', {sourcemaps: true})
+      .pipe(sass().on('error', sass.logError))
+      .pipe(postcss([autoprefixer(), cssnano()])) 
+      //autoprefixer will add browser prefixes to support old browsers
+      .pipe(gulp.dest('./dist', {sourcemaps: '.'}));
+})
 
-//Javascript task
-function jsTask() {
-    return src('app/js/script.js', { sourcemaps: true })
-    .pipe (babel({presets: ['@babel/preset-env']}))
-    //allow older browsers to support javascript
-    .pipe(terser())
-    //terser minifies JS file
-    .pipe(dest('dist', { sourcemaps: '.' }));
-    //destination script.js file goes into dist folder, along with its map
-}
+//CSS Task
+gulp.task('css', function () {
+  return gulp.src('./app/css/*.css', {sourcemaps: true})
+      .pipe(uglifycss({"uglyComments": true}))
+      .pipe(gulp.dest('./app/css', {sourcemaps: '.'}));
+});
+
+//JS Task
+gulp.task('js', function () {
+  return gulp.src('./app/js/script.js', {sourcemaps: true})
+  .pipe(terser())
+  .pipe (babel({presets: ['@babel/preset-env']}))
+   //allow older browsers to support javascript
+  .pipe(gulp.dest('./dist/', {sourcemaps: '.'}))
+});
 
 //Browsersync start
 function browserSyncServer(cb) {
@@ -51,16 +54,29 @@ function browserSyncServer(cb) {
     cb();
   }
 
+gulp.task('run', gulp.series('scss', 'css', 'js'));
 
 //Watch task will keep watch for any changes then call BrowserSyncReload
 function watchTask(){
     watch('*.html', browserSyncReload);
-    watch(
-        ['app/scss/**/*.scss', 'app/**/*.js'],
-        series(scssTask, jsTask, browserSyncReload)
-    );
+    
 }
 
+
+//Dynamic watch task - watch change in SCSS files
+gulp.task('watch', function () {
+    gulp.watch('*.html', browserSyncReload);
+    gulp.watch(['app/scss/**/*.scss', 'app/**/*.js'], series(scssTask, jsTask, browserSyncReload));
+});
+
+gulp.task('flask', function(){
+  var spawn = process.spawn;
+  console.info('Starting flask server');
+  var PIPE = {stdio: 'inherit'};
+  spawn('python', ['manage.py','runserver'], PIPE);
+});
+
+
 //Execute all tasks in series (at the same time)
-exports.default = series(scssTask, jsTask, browserSyncServer, watchTask);
+gulp.task('default', gulp.series('run', 'watch'));
 
