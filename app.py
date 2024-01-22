53bad95b36d63ceb05b2f3abb48808b90a320c54
Kenny Guo <110429254+kennykguo@users.noreply.github.com>
2024-01-21 21:10:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,1 @@
-import os
-import locale
-import twitter_test
-locale.setlocale(locale.LC_ALL, '')
-from flask import Flask, flash, redirect, render_template, request, session, url_for
-from werkzeug.security import check_password_hash, generate_password_hash
 
-
-app = Flask(__name__) #create a Flask instance
-
-@app.route('/', methods=['GET'])
-def input():
-    if request.method == 'GET':
-        return render_template('index.html')
-    
-
-auth = twitter_test.OAuthHandler("Bearer Token here")
-api = twitter_test.API(auth)
